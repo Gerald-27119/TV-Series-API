18659f547467dff275f78dad664fa2545c4407a6
Adam Langmesser <s27119@pjwstk.edu.pl>
2024-01-13 16:23:51 +0100

    robimy front

---
+++ series-web/src/test/java/com/example/seriesweb/SeriesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seriesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
